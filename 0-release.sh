ff295d2a681bc3b0e6e014c2a0b7398eb0adf6ff
gabriel.calero <gabrielbolso19@gmail.com>
2024-12-12 15:46:32 -0500

    made same changes

--- 0-release.sh
+++ 0-release.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
-
+#! /bin/bash 
 lsb_release -a | head -n 1 | cut -c 17-22
+echo
---
+++ 4-root_process.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ -n $1 ]
+then ps -eo user,pid,ppid,vsz,rss,tty,stat,start,time,"command" | grep -e "^$1" 
+fi
+
