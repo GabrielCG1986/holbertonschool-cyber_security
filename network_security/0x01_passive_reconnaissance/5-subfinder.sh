de2b135ddcd3461143b906c7c10a7e6bab9b95e7
gabriel.calero <gabrielbolso19@gmail.com>
2024-12-19 13:41:10 -0500

    changed some file names

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -nW -oI | awk -F, '{print $1 "," $2}' | tee output.txt | cut -d',' -f 1
+subfinder -d $1 -nW -oI | awk -F, '{print $1 "," $2}' | tee $1.txt | cut -d',' -f 1
