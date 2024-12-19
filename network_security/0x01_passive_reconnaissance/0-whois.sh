57b862edba7c96e847ad4b917f7e4ba76d38c389
gabriel.calero <gabrielbolso19@gmail.com>
2024-12-19 18:23:42 -0500

    changed some file names

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | grep -E 'Registrant|Admin|Tech' | awk -F': ' '{ print $1 "," $2}' | cat > $1.csv 
+whois $1 | grep -E '^(Registrant|Admin|Tech)' | awk -F': ' '{ print $1 "," $2}' | cat > $1.csv 
